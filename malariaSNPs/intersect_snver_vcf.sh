113a48205d27c2a0f18e35b7ebf5054bc328738c
J S Penington <penington.j@wehi.edu.au>
2017-11-07 15:23:28 +1100

    add SNVer commands to shell script

--- malariaSNPs/intersect_snver_vcf.sh
+++ malariaSNPs/intersect_snver_vcf.sh
@@ -1,23 +1,46 @@
 ## Jocelyn Sietsma Penington
 ## October 2017
+
+## Use SNVerPool to find single nucleotide variations in the joint Pf bam files
+## for each set, SNVerIndividual for the merged parent bam.
+## Use bedtools subtract to extract events in daughter strains that are not in parents
+## Use bedtools intersect, and grep, to find and count events in coding regions.
+
+## On 9 Oct 2017 SNVer version is 0.5.3
+
+PAPDIR=/wehisan/bioinf/bioinf-data/Papenfuss_lab/projects
+BASEDIR=$PAPDIR/malaria/cowman_lab/drug_resistance
+CURDIR=$BASEDIR/currentProject
+ALIGNDIR=$CURDIR/alignment
+VARDIR=$CURDIR/variants
+REFDIR=$PAPDIR/reference_genomes/plasmodium/PlasmoDB-29_Pfalciparum3D7
+SNVERD=/usr/local/bioinfsoftware/SNVer/current
+
+moday="$(date +"%d%b")"
+SNVDIR=$VARDIR/snverP${moday}
+mkdir $SNVDIR
+
+for s in A B D E
+do
+   java -jar $SNVERD/SNVerPool.jar -i $ALIGNDIR -c $ALIGNDIR/${s}pool.txt \
+        -r $REFDIR/PlasmoDB-29_Pfalciparum3D7_Genome.fasta \
+        -o $OUTDIR/$s
+done
+
+
+java -jar $SNVERD/SNVerIndividual.jar -i $ALIGNDIR/3D7-merge-B2_S1-F4_S4.bam  \
+     -r $REFDIR/PlasmoDB-29_Pfalciparum3D7_Genome.fasta -n 1  \
+     -o $OUTDIR/mergedParent
+
 ## Take output of SNVerP for drug-resistant and parent 3D7 strains
 ## compared to reference, find the difference between resistant and parent strains. 
 ## Based on annotateIntersect.sh in malaria/vaccine/output/SNVerI and 
 ## intersect_snver_vcf.sh in drug_resistance/analysis_tools/
-## SNVer command is in analysis_tools/snver.sh
+## (SNVer commands were in separate script, now inserted above.)
+
 ## Latest gff file from PlasmoDB (v29) does not need cleaning for this application.
 
-### Add section to filter by number of pool samples the event is present in?    
-### There are 3 in A, 4 in each of the others. No, easier in R
-
-PAPDIR=/wehisan/bioinf/bioinf-data/Papenfuss_lab/projects
-BASEDIR=$PAPDIR/malaria/cowman_lab/drug_resistance
-REFDIR=$PAPDIR/reference_genomes/plasmodium/PlasmoDB-29_Pfalciparum3D7
-GILDIR=$BASEDIR/GilsonT210617
-ALIGNDIR=$GILDIR/alignment
-TOOLDIR=$GILDIR/analysis_tools
-SNVDIR=$GILDIR/variants/snverP09Oct
-
+## Check date in name of SNVDIR !
 export OUTDIR=$SNVDIR/removedParentVariants
 mkdir $OUTDIR
 
@@ -38,7 +61,7 @@ do
    
 	# Intersect with reference annotations in both orders, to get SNVs that lie in features, 
 	# and features that contain SNVs.  For the SNV-first version use -u so that results 
-	# aren't repeated for each description/feature in gff. This filtering will be repeated 
+	# aren't repeated for each description/feature in gff. This filtering is repeated 
 	# in maldrugGilsonR/readSNVer.R
     # Count events with grep -c
    grep '^#' $OUTDIR/${s}-parent.filter.vcf > $OUTDIR/${s}_SNPgff.vcf
