27f711a5f3ed8873649d4463a3548cc4df301e18
JocelynSP <penington.j@wehi.edu.au>
2019-07-15 15:04:14 +1000

    change relpos to count in transcript direction

--- annotateSNP.py
+++ annotateSNP.py
@@ -6,7 +6,7 @@ Outputs a list of annotations to stdout of either Synonymous or the aa change
 Author: Jan Schroeder
 Modified: Jocelyn Sietsma Penington March 2016 to use on malaria vaccine project
 
-My vcf files do not have strand information, my GFF does
+My vcf files do not have strand information, my GFF does.
 Original code assumes vcf reads have a start and end. I am not going to read indel files, 
 as they are unable to be synonomous if in a coding region, so all changes are 1-1
 Where there is more than one alternate allele in the vcf, all changes in coding sequences 
@@ -14,6 +14,16 @@ are reported. (Loop is inside test for exon)
 
 'exons' extraction actually extracts features of type 'CDS' because these include the 
 frame.
+
+BUG: If frame of CDS > 0 the first 'frame' bases are not checked.
+BUG: Each SNV is checked independently. If 2 fall in the same codon, it will be
+reported as 2 (incorrect) AA changes from the reference
+ 
+TODO: It would be good to also output AA position in transcript. 
+This requires concatenating all the exons in a transcript, in a strand-aware manner. 
+HTSeq gff-reader makes the attributes available, e.g. genomicfeature.attr[‘Parent’]
+CDS in same transcript have same parent
+
 """
 
 #!/usr/bin/env python
@@ -22,6 +32,7 @@ import sys
 import HTSeq
 import itertools
 import string
+import math
 
 def rc(dna):
  complements = string.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
@@ -29,16 +40,16 @@ def rc(dna):
  return rcseq
 
 amino = {"TTT":"F|Phe","TTC":"F|Phe","TTA":"L|Leu","TTG":"L|Leu","TCT":"S|Ser","TCC":"S|Ser",
-              "TCA":"S|Ser","TCG":"S|Ser", "TAT":"Y|Tyr","TAC":"Y|Tyr","TAA":"*|Stp","TAG":"*|Stp",
-              "TGT":"C|Cys","TGC":"C|Cys","TGA":"*|Stp","TGG":"W|Trp", "CTT":"L|Leu","CTC":"L|Leu",
-              "CTA":"L|Leu","CTG":"L|Leu","CCT":"P|Pro","CCC":"P|Pro","CCA":"P|Pro","CCG":"P|Pro",
-              "CAT":"H|His","CAC":"H|His","CAA":"Q|Gln","CAG":"Q|Gln","CGT":"R|Arg","CGC":"R|Arg",
-              "CGA":"R|Arg","CGG":"R|Arg", "ATT":"I|Ile","ATC":"I|Ile","ATA":"I|Ile","ATG":"M|Met",
-              "ACT":"T|Thr","ACC":"T|Thr","ACA":"T|Thr","ACG":"T|Thr", "AAT":"N|Asn","AAC":"N|Asn",
-              "AAA":"K|Lys","AAG":"K|Lys","AGT":"S|Ser","AGC":"S|Ser","AGA":"R|Arg","AGG":"R|Arg",
-              "GTT":"V|Val","GTC":"V|Val","GTA":"V|Val","GTG":"V|Val","GCT":"A|Ala","GCC":"A|Ala",
-              "GCA":"A|Ala","GCG":"A|Ala", "GAT":"D|Asp","GAC":"D|Asp","GAA":"E|Glu",
-              "GAG":"E|Glu","GGT":"G|Gly","GGC":"G|Gly","GGA":"G|Gly","GGG":"G|Gly"}
+  "TCA":"S|Ser","TCG":"S|Ser", "TAT":"Y|Tyr","TAC":"Y|Tyr","TAA":"*|Stp","TAG":"*|Stp",
+  "TGT":"C|Cys","TGC":"C|Cys","TGA":"*|Stp","TGG":"W|Trp", "CTT":"L|Leu","CTC":"L|Leu",
+  "CTA":"L|Leu","CTG":"L|Leu","CCT":"P|Pro","CCC":"P|Pro","CCA":"P|Pro","CCG":"P|Pro",
+  "CAT":"H|His","CAC":"H|His","CAA":"Q|Gln","CAG":"Q|Gln","CGT":"R|Arg","CGC":"R|Arg",
+  "CGA":"R|Arg","CGG":"R|Arg", "ATT":"I|Ile","ATC":"I|Ile","ATA":"I|Ile","ATG":"M|Met",
+  "ACT":"T|Thr","ACC":"T|Thr","ACA":"T|Thr","ACG":"T|Thr", "AAT":"N|Asn","AAC":"N|Asn",
+  "AAA":"K|Lys","AAG":"K|Lys","AGT":"S|Ser","AGC":"S|Ser","AGA":"R|Arg","AGG":"R|Arg",
+  "GTT":"V|Val","GTC":"V|Val","GTA":"V|Val","GTG":"V|Val","GCT":"A|Ala","GCC":"A|Ala",
+  "GCA":"A|Ala","GCG":"A|Ala", "GAT":"D|Asp","GAC":"D|Asp","GAA":"E|Glu",
+  "GAG":"E|Glu","GGT":"G|Gly","GGC":"G|Gly","GGA":"G|Gly","GGG":"G|Gly"}
 
 def amino_change(aa1, aa2):
   if len(aa1) != len(aa2): 
@@ -70,39 +81,43 @@ exons = HTSeq.GenomicArray( "auto", stranded=False, typecode='O' )
 
 for feature in gff_file:
   if feature.type == "CDS": 
-    exons[ feature.iv ] = feature
+    exons[ feature.iv ] = feature  # .iv is GenomicInterval
     
 print "# Chrom\tPos\tPos in CDS\tBase change\tAA change\tgene ID"
 vcfr = HTSeq.VCF_Reader( sys.argv[3])
 
 for vc in vcfr:
-	l = exons[ vc.pos ] 
-	# l.iv.start is base before 1st base of CDS
-	if not l==None and not vc.pos.start==l.iv.start: 
-		refseq = sequences[l.iv.chrom].seq[l.iv.start:l.iv.end]
-		if len(refseq) != l.iv.length:
+	vCDS = exons[ vc.pos ] 
+	# vCDS.iv.start is base before 1st base of CDS
+	if not vCDS==None and not vc.pos.start==vCDS.iv.start: 
+		refseq = sequences[vCDS.iv.chrom].seq[vCDS.iv.start:vCDS.iv.end]
+		if len(refseq) != vCDS.iv.length:
 			print "Error in sequence lengths"
-		relpos = vc.pos.start - l.iv.start 
+		relpos = vc.pos.start - vCDS.iv.start 
 		# if variant is 1st base of exon, relpos=1
 		if refseq[relpos-1] != vc.ref:
-			print "Error: Reference Base not according to SNP!"
+			print "ERROR: Reference Base not according to SNP"
 		for alt in vc.alt:        # vc.alt is a list of alternative base(s)
 			alternateSeq = refseq[0:relpos-1] + alt + refseq[relpos:]    
 			if len(refseq) != len(alternateSeq):
-				print 'ALARM - length error when substituting alt for ref'
-				print vc, relpos, l
-			if l.iv.strand == "-":
+				print 'WARNING - length error when substituting alt for ref'
+				print vc, relpos, vCDS
+			if vCDS.iv.strand == "-":
 				refseq = rc(refseq)
 				alternateSeq = rc(alternateSeq)
-			aa = protein(refseq, l.frame)
-			aa2 = protein(alternateSeq, l.frame)
+				relpos = vCDS.iv.end + 1 - vc.pos.start 
+# 				aapos = int( math.ceil( (relpos + vCDS.frame)/3.0 ) )
+# 			else:
+# 				aapos = int( math.ceil( (relpos - vCDS.frame)/3.0 ) )
+			aa = protein(refseq, vCDS.frame)
+			aa2 = protein(alternateSeq, vCDS.frame)
 			if aa != aa2:
 				change = amino_change(aa,aa2)
 				ch="%s->%s" %( change[0], change[1])
 			else:
 				ch = 'Synonymous'
 			snp = "%s->%s" %( vc.ref, alt)
-			list = [vc.chrom, str(vc.pos.start), str(relpos), snp, ch, l.name]
+			list = [vc.chrom, str(vc.pos.start), str(relpos), snp, ch, vCDS.name]
 			print '\t'.join(list)
 	else:
 		ch = 'X'
